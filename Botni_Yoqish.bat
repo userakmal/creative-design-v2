b1a076efb3f05932925c912ba4854e0e535dea56
userakmal <akmalgulomov03@gmail.com>
2026-03-26 11:39:55 +0500

    feat: Add Telegram video downloader bot, local video API, and a new downloader page.

---
+++ Botni_Yoqish.bat
@@ -0,0 +1,9 @@
+@echo off
+title Telegram Video Downloader Bot
+color 0B
+echo ===================================================
+echo     Telegram Video Downloader Bot ishga tushyapti...
+echo     Iltimos, ushbu oynani yopmang!
+echo ===================================================
+node bot.cjs
+pause
